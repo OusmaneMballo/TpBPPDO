0f4570f735eac452869b71c96e7df0c407741536
OusmaneMballo <ousmanemballo576@gmail.com>
2020-07-06 01:18:38 +0000

    CompteDAO

--- DAO/CompteDAO.php
+++ DAO/CompteDAO.php
@@ -1,7 +1,33 @@
 <?php
 
-
+include '../DAO/AccessdbDAO.php';
 class CompteDAO
 {
 
+    /**
+     * CompteDAO constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function add($compte)
+    {
+        $db=new AccessdbDAO();
+        $requete="INSERT INTO `compte_client`(`id`, `numeroCte`, `clerib`, 
+                `agence_id`, `solde`, `etat`, `cltphy_id`, `cltmoral_id`, 
+                `datecrea`, `dateferme`, `datefertempo`, `datereouv`, 
+                `id_type`)
+                 VALUES (null,?,?,?,?,?,?,?,?,?,?,?,?);";
+        $cnx=$db->getConnexion();
+        $prepare=$db->MAJ($cnx,$requete);
+        try {
+            $result=$prepare->execute(array($compte->getNumero(),$compte->getCleRipe(),
+                1,$compte->getSolde(),'Actif', $compte->getClientP(), $compte->getClientM(), date('d-m-y'),
+                null, null, null, $compte->getTypeCompte()));
+            return $cnx->lastInsertId();
+        }catch(Exception  $e){
+            return null;
+        }
+    }
 }
